f26473a8b9f804396ca034f1bebeb771cfe7b264
litAlexCuesta <alejandro.cuesta.trapote@colegiolitterator.com>
2023-10-09 16:26:04 +0200

    Update gitHub.java

--- Interfaces/src/gitHub/gitHub.java
+++ Interfaces/src/gitHub/gitHub.java
@@ -3,7 +3,7 @@ package gitHub;
 public class gitHub {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World prueba");
+		System.out.println("Hello World prueba v2");
 	}
 
 }
