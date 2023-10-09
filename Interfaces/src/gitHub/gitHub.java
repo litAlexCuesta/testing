a9aa2024d969dcfa3a001cc44ed65ae48dfbc7ab
Alex <alejandro.cuesta.trapote@colegiolitterator.com>
2023-10-09 16:12:31 +0200

    prueba

---
+++ Interfaces/src/gitHub/gitHub.java
@@ -0,0 +1,9 @@
+package gitHub;
+
+public class gitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Interfaces/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alex
+ *
+ */
+module Interfaces {
+}
