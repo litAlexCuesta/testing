eef1baa37e582dd3e371a75af7b677fb6d55c2e3
litAlexCuesta <alejandro.cuesta.trapote@colegiolitterator.com>
2023-10-11 17:14:54 +0200

    PRUEBA_V6

--- Interfaces/src/gitHub/gitHub.java
+++ Interfaces/src/gitHub/gitHub.java
@@ -3,7 +3,7 @@ package gitHub;
 public class gitHub {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World prueba v5");
+		System.out.println("Hello World prueba v6");
 	}
 
 }
